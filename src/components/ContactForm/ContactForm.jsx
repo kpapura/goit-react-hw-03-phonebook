88bd48fef4c5b3a8d3e92d77970bfac08538c2d7
kpapura <125825500+kpapura@users.noreply.github.com>
2023-06-02 18:53:36 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -27,6 +27,8 @@ class ContactForm extends Component {
           name="name"
           placeholder="Enter name"
           value={name}
+             minLength="2"
+          maxLength="30"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           onChange={this.handleChange}
@@ -39,6 +41,8 @@ class ContactForm extends Component {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           value={number}
+          minLength="5"
+          maxLength="20"
           onChange={this.handleChange}
           required
         />
